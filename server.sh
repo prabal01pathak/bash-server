642f4f25cc937d455f6e034556e98fae43582872
Prabal Pathak <prabal01pathak@gmail.com>
2023-09-25 23:43:49 +0530

    commit added get / endpoint

--- server.sh
+++ server.sh
@@ -5,15 +5,20 @@ rm -rf response
 mkfifo response
 
 function handleRequest() {
-    # TODO: Do the stuff
     while read line; do
         trline=`echo $line | tr -d '[\r\n]'`
         echo "tr line is: $trline"
-        if [ -z $trline ]; then
-            break
-        fi
+        [ -z "$trline" ] && break
+        HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+         [[ "$trline" =~ $HEADLINE_REGEX ]] &&
+            REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
+        echo "Request is: $REQUEST"
     done
-    echo  -e "HTTP/1.1 200\r\nContent-Type:text/html\r\n\r\n<h1>PONG<h2>" > response
+    case "$REQUEST" in 
+        "GET /") RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>PONG</h1>";;
+              *) RESPONSE="HTTP/1.1 400 NotFound\r\n\r\n\r\nNot Found";; 
+    esac
+    echo -e  $RESPONSE > response
 }
 
 echo "Listening on 3000 ...."
